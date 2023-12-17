4ae7448bb83a86193d7e931bebf3cf0867c473b9
Lawrence Craft <lawrence.craft@gmail.com>
2023-12-17 02:49:21 +0000

    day10

--- day10/day10.py
+++ day10/day10.py
@@ -1,7 +1,7 @@
 from collections import deque
 
 
-def pad_input(input: list[str]):
+def pad_input(input: list[str]) -> list[str]:
     padded_input = []
     padded_input.append("".join("." for _ in range(len(input[0]) + 2)))
     padded_input.extend(f".{i}." for i in input)
@@ -33,12 +33,31 @@ def connected_nodes(grid: list[str], row: int, col: int):
         yield (row + 1, col)
 
 
-def print_grid(grid: list[list[int | None]]):
+SYM_2_OFFSET = {
+    "|": [(1, 0), (-1, 0)],
+    "-": [(0, 1), (0, -1)],
+    "F": [(1, 0), (0, 1)],
+    "J": [(0, -1), (-1, 0)],
+    "7": [(0, -1), (1, 0)],
+    "L": [(0, 1), (-1, 0)],
+}
+
+
+def possible_nodes(row: int, col: int, symbol: str):
+    for dr, dc in SYM_2_OFFSET[symbol]:
+        yield (dr + row, dc + col)
+
+
+def print_grid(grid: list[list[int | None]], padding=1):
     for i in grid:
-        print("".join("." if j == None else str(j) for j in i))
+        print(
+            "".join(
+                ".".rjust(padding) if j == None else str(j).rjust(padding) for j in i
+            )
+        )
 
 
-def find_furthest(grid: list[str]) -> int:
+def find_furthest(grid: list[str]) -> tuple[int, list[list[int | None]]]:
     visited: list[list[int | None]] = [[None for _ in r] for r in grid]
     start_row, start_col = find_start(grid)
 
@@ -55,25 +74,58 @@ def find_furthest(grid: list[str]) -> int:
 
         next_distance = current_distance + 1
 
-        for node_row, node_col in connected_nodes(grid, current_row, current_col):
+        next_gen = (
+            connected_nodes(grid, current_row, current_col)
+            if current_distance == 0
+            else possible_nodes(
+                current_row, current_col, grid[current_row][current_col]
+            )
+        )
+
+        for node_row, node_col in next_gen:
             if visited[node_row][node_col] is not None:
                 continue
 
             visited[node_row][node_col] = next_distance
             to_visit.append((next_distance, node_row, node_col))
 
-    # print_grid(visited)
-    return highest_seen
+    return highest_seen, visited
 
 
 def part1(input: list[str]):
     padded_input = pad_input(input)
-    print(find_furthest(padded_input))
-    # print(list(connected_nodes(padded_input, 3, 4)))
+    print(find_furthest(padded_input)[0])
 
 
 def part2(input: list[str]):
-    pass
+    padded_input = pad_input(input)
+    padded_input = [list(s) for s in padded_input]
+    _, visited = find_furthest(padded_input)
+    num_inside = 0
+
+    for row_idx, row in enumerate(padded_input):
+        for col_idx, col in enumerate(row):
+            if visited[row_idx][col_idx] is not None:
+                continue
+
+            inside = False
+            current_col = col_idx + 1
+
+            for right in range(current_col, len(row)):
+                if visited[row_idx][right] is None:
+                    continue
+
+                if row[right] in ("F", "7", "|", "S"):
+                    inside = not inside
+
+            if inside:
+                padded_input[row_idx][col_idx] = "I"
+                num_inside += 1
+            else:
+                padded_input[row_idx][col_idx] = "O"
+    # print_grid(visited, padding=3)
+    # print_grid(padded_input)
+    print(num_inside)
 
 
 def main():
